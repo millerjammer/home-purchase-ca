6c1dab048dc899b9ad044774bce08e180df92068
millerjammer <millerjammer@gmail.com>
2026-03-08 09:24:55 -0700

    back to regular html and js

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,8 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-
+  
   entry: "./src/home-purchase-ca.js",
 
   output: {
